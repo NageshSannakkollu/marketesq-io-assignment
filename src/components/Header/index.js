99ff8bdd76f940a23a60457d06ea1f9bb6b08af6
NageshSannakkollu <122163329+NageshSannakkollu@users.noreply.github.com>
2024-11-10 12:38:13 +0530

    index.js

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom"
 import "./index.css"
 const Header = () => (
     <nav className="nav-header-container">
-        <Link to="https://marketesq-io-assignment-gamma.vercel.app/" className="link-item"><h4>Brisphere</h4></Link>
+        <Link to="https://marketesq-io-assignment-gamma.vercel.app" className="link-item"><h4>Brisphere</h4></Link>
         <ul className="nav-header-links">
             <li>Discover</li>
             <li>Services</li>
